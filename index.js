875a94128d480fdad5be55f32afc82364a2e2e3c
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-03 18:37:54 -0300

    teste final 2

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.get("/", async (req, res) => {
 
 const PORT = process.env.PORT || 5050;
 
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: true }));
 
 // app.use("/");
 
-app.listen(PORT, () => console.log(`Server is running in port ${PORT} or http://localhost:5050/api/items`))
+app.listen(PORT, () => console.log(`Server is running in port ${PORT} or http://localhost:5050`))
