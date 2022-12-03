ba2bf26561f6596a57a06fc40b8b863706eda723
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-03 17:54:56 -0300

    teste vercel 5

--- api/items.js
+++ api/items.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
-  const client = await auth.getClient();
+  // const client = await auth.getClient();
   // const googleSheets = google.sheets({ version: "v4", auth: client });
   const spreadsheetId = '1EUpzlHqk4IFaoC0bDMWtTw26wuf14YBQzXR6nyz0QtQ';
 
