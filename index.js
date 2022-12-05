09c0dd356b3e404a630ad22905536352b0f50f5d
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-05 20:05:32 -0300

    add append

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ app.use(cors());
 const items = require("./api/items");
 const users = require("./api/users");
 const chars = require("./api/chars");
+const addUser = require("./api/addUser");
 
 const PORT = process.env.PORT || 5050;
 
-app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/api/items", items);
 app.use("/api/users", users);
 app.use("/api/chars", chars);
+app.use("/api/addUser", addUser);
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT} or 
 http://localhost:5050/api/items
