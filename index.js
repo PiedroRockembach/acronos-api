d0f9907715b1f6a4fff56bd2cb30571c03d9d051
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-15 23:50:22 -0300

    aaa

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const { google } = require('googleapis')
 const app = express();
 
-app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://acronos-rpg.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
