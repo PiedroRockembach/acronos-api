c55ff2bb3bffa8c24b34fc3471b7b40522f593d0
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-15 23:33:22 -0300

    muda cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { google } = require('googleapis')
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   app.use(cors()); 
   next();
