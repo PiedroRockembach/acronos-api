95490af38eefc84884c41719e84f92131850bf3d
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-16 00:02:52 -0300

    add localhost sem barra

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { google } = require('googleapis')
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://acronos-rpg.vercel.app, http://localhost:3000/");
+  res.header("Access-Control-Allow-Origin", "https://acronos-rpg.vercel.app, http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   app.use(cors()); 
