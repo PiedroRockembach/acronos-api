dc53318ec45b8c462bd39fe6e760987e2faec832
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-21 22:59:55 -0300

    sla

--- api/invites.js
+++ api/invites.js
@@ -61,7 +61,7 @@ router.post("/", async (req, res) => {
       resource: {
         range: `usuarios!A${id}:E${id}`,
         values: [
-          [cur.nome, cur.login, cur.senha, cur.id, cur.email ? JSON.stringify([ ...email, jsonInvite]) :  JSON.stringify([jsonInvite])]
+          [cur.nome, cur.login, cur.senha, cur.id, JSON.stringify([ ...email, jsonInvite])]
         ]
       }
       
